4cebed1b829c5e5892007a0d51429025cef79488
Langning Chen <chenlangning2009@outlook.com>
2025-04-25 13:57:31 +0800

    Innovate for the future

---
+++ boat.ino
@@ -0,0 +1,46 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+#define CE_PIN 4
+#define CSN_PIN 9
+
+RF24 radio(CE_PIN, CSN_PIN);
+const byte address[6] = "765FA";
+
+void setup()
+{
+    Serial.begin(115200);
+    delay(1000);
+
+    // 初始化 SPI
+    SPI.begin(10, 12, 11, 9); // SCK, MISO, MOSI, SS
+
+    Serial.println("初始化 NRF24L01...");
+    if (!radio.begin())
+    {
+        Serial.println("NRF24L01 初始化失败！");
+        while (1)
+            ;
+    }
+
+    radio.setChannel(76);
+    radio.setDataRate(RF24_250KBPS);
+    radio.setPALevel(RF24_PA_LOW);
+    radio.setAutoAck(true);
+    radio.openReadingPipe(0, address);
+    radio.startListening();
+
+    Serial.println("接收端初始化完成，等待数据...");
+}
+
+void loop()
+{
+    if (radio.available())
+    {
+        char receivedText[32] = {0};
+        radio.read(&receivedText, sizeof(receivedText));
+        Serial.print("接收到数据: ");
+        Serial.println(receivedText);
+    }
+}
