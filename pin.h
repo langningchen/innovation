b558a0f0f22044a681a2094893357b4dea47f4a3
Langning Chen <chenlangning2009@outlook.com>
2025-04-27 13:40:09 +0800

    Add GPL v3 license and copyright notice to all source files

--- pin.h
+++ pin.h
@@ -1,3 +1,20 @@
+// Copyright (C) 2025 langningchen
+//
+// This file is part of innovation.
+//
+// innovation is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// innovation is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with innovation.  If not, see <https://www.gnu.org/licenses/>.
+
 // NRF24L01
 #define PIN_MISO 12
 #define PIN_MOSI 11
