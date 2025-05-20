a6d6d079b00cede0d5c158036c40d8bf6d62771d
Langning Chen <chenlangning2009@outlook.com>
2025-05-20 19:00:58 +0800

    Fix header

--- mpu6050.hpp
+++ mpu6050.hpp
@@ -15,23 +15,6 @@
 // You should have received a copy of the GNU General Public License
 // along with innovation.  If not, see <https://www.gnu.org/licenses/>.
 
-// Copyright (C) 2025 Langning Chen
-//
-// This file is part of innovation.
-//
-// innovation is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// innovation is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with innovation.  If not, see <https://www.gnu.org/licenses/>.
-
 #pragma once
 
 #include <Adafruit_MPU6050.h>
